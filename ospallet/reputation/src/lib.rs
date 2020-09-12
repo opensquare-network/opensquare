c427e5cc8116b2a9a130f0ad3f303e5af019f8e5
Yongfeng LI <wliyongfeng@gmail.com>
2020-09-12 20:45:32 +0800

    Add unit test for reputation pallet (#25)

--- ospallet/reputation/src/lib.rs
+++ ospallet/reputation/src/lib.rs
@@ -63,3 +63,92 @@ impl<T: Trait> ReputationBuilder<T::AccountId> for Module<T> {
         };
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use frame_support::{impl_outer_origin, parameter_types, weights::Weight};
+    use sp_core::H256;
+    use sp_io::TestExternalities;
+    use sp_runtime::{
+        testing::Header,
+        traits::{BlakeTwo256, IdentityLookup},
+        Perbill,
+    };
+
+    pub fn new_test_ext() -> TestExternalities {
+        let t = frame_system::GenesisConfig::default()
+            .build_storage::<Test>()
+            .unwrap();
+        TestExternalities::new(t)
+    }
+
+    impl_outer_origin! {
+        pub enum Origin for Test where system = frame_system {}
+    }
+
+    #[derive(Clone, Eq, PartialEq)]
+    pub struct Test;
+    parameter_types! {
+        pub const BlockHashCount: u64 = 250;
+        pub const MaximumBlockWeight: Weight = 1024;
+        pub const MaximumBlockLength: u32 = 2 * 1024;
+        pub const AvailableBlockRatio: Perbill = Perbill::one();
+    }
+    impl frame_system::Trait for Test {
+        type BaseCallFilter = ();
+        type Origin = Origin;
+        type Index = u64;
+        type BlockNumber = u64;
+        type Call = ();
+        type Hash = H256;
+        type Hashing = BlakeTwo256;
+        type AccountId = u64;
+        type Lookup = IdentityLookup<Self::AccountId>;
+        type Header = Header;
+        type Event = ();
+        type BlockHashCount = BlockHashCount;
+        type MaximumBlockWeight = MaximumBlockWeight;
+        type DbWeight = ();
+        type BlockExecutionWeight = ();
+        type ExtrinsicBaseWeight = ();
+        type MaximumExtrinsicWeight = MaximumBlockWeight;
+        type AvailableBlockRatio = AvailableBlockRatio;
+        type MaximumBlockLength = MaximumBlockLength;
+        type Version = ();
+        type ModuleToIndex = ();
+        type AccountData = ();
+        type OnNewAccount = ();
+        type OnKilledAccount = ();
+        type SystemWeightInfo = ();
+    }
+
+    impl Trait for Test {
+        type Event = ();
+    }
+
+    type Reputation = Module<Test>;
+
+    #[test]
+    fn add_behavior_score_by_behavior_works() {
+        new_test_ext().execute_with(|| {
+            let account = 1;
+            Reputation::add_behavior_score_by_behavior(
+                &account,
+                &Behavior::BountyResolve(BountyResolveCollaborationResult::Success),
+            );
+
+            let score = Reputation::behavior_score(&account);
+            assert_eq!(10, score);
+        });
+    }
+
+    #[test]
+    fn get_behavior_score_works() {
+        let score = Reputation::get_behavior_score(&Behavior::BountyResolve(
+            BountyResolveCollaborationResult::Fail,
+        ));
+        assert_eq!(-2, score);
+    }
+}
