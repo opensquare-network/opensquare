4f47465c56acf979600454b903d4329c2c73b707
koushiro <koushiro.cqx@gmail.com>
2020-09-18 11:31:47 +0800

    Remove useless code in ospallet_support
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- ospallet/support/src/macros.rs
+++ ospallet/support/src/macros.rs
@@ -1,131 +1,51 @@
-pub use log::*;
+pub const RUNTIME_TARGET: &str = "runtime";
 
-#[cfg(feature = "std")]
 #[macro_export]
-macro_rules! str {
-    ( $x:expr ) => {
-        $crate::x_std::Str(&$crate::x_std::as_string(&$x))
-    };
-}
-
-#[cfg(not(feature = "std"))]
-#[macro_export]
-macro_rules! str {
-    ( $x:expr ) => {
-        &$x
-    };
+macro_rules! error {
+    (target: $target:expr, $($arg:tt)+) => (
+        frame_support::debug::error!(target: $target, $($arg)+);
+    );
+    ($($arg:tt)+) => (
+        $crate::error!(target: RUNTIME_TARGET, $($arg)+);
+    )
 }
 
 #[macro_export]
-macro_rules! token {
-    ( $x:expr ) => {
-        $crate::str!($x)
-    };
+macro_rules! warn {
+    (target: $target:expr, $($arg:tt)+) => (
+        frame_support::debug::warn!(target: $target, $($arg)+);
+    );
+    ($($arg:tt)+) => (
+        $crate::warn!(target: RUNTIME_TARGET, $($arg)+);
+    )
 }
 
-#[cfg(feature = "std")]
 #[macro_export]
-macro_rules! try_addr {
-    ( $x:expr ) => {{
-        $crate::x_std::Str(&$crate::x_std::as_addr(&$x))
-    }};
+macro_rules! info {
+    (target: $target:expr, $($arg:tt)+) => (
+        frame_support::debug::info!(target: $target, $($arg)+);
+    );
+    ($($arg:tt)+) => (
+        $crate::info!(target: RUNTIME_TARGET, $($arg)+);
+    )
 }
 
-#[cfg(not(feature = "std"))]
 #[macro_export]
-macro_rules! try_addr {
-    ( $x:expr ) => {{
-        &$x
-    }};
+macro_rules! debug {
+    (target: $target:expr, $($arg:tt)+) => (
+        frame_support::debug::debug!(target: $target, $($arg)+);
+    );
+    ($($arg:tt)+) => (
+        $crate::debug!(target: RUNTIME_TARGET, $($arg)+);
+    )
 }
 
-#[cfg(feature = "std")]
 #[macro_export]
-macro_rules! try_hex {
-    ( $x:expr ) => {{
-        $crate::x_std::Str(&$crate::x_std::try_hex_or_str(&$x))
-    }};
-}
-
-#[cfg(not(feature = "std"))]
-#[macro_export]
-macro_rules! try_hex {
-    ( $x:expr ) => {{
-        &$x
-    }};
-}
-
-#[cfg(feature = "std")]
-#[macro_export]
-macro_rules! ensure_with_errorlog {
-    ( $x:expr, $y:expr, $($arg:tt)*) => {{
-        if !$x {
-            $crate::error!("{:?}|{}", $y, format!($($arg)*));
-            $crate::fail!($y);
-        }
-    }}
-    }
-
-#[cfg(not(feature = "std"))]
-#[macro_export]
-macro_rules! ensure_with_errorlog {
-    ( $x:expr, $y:expr, $($arg:tt)*) => {{
-        if !$x {
-            $crate::fail!($y);
-        }
-    }};
-}
-
-pub mod log {
-    pub const RUNTIME_TARGET: &str = "runtime";
-
-    #[macro_export]
-    macro_rules! error {
-        (target: $target:expr, $($arg:tt)+) => (
-            frame_support::debug::error!(target: $target, $($arg)+);
-        );
-        ($($arg:tt)+) => (
-            $crate::error!(target: "runtime", $($arg)+);
-        )
-    }
-
-    #[macro_export]
-    macro_rules! warn {
-        (target: $target:expr, $($arg:tt)+) => (
-            frame_support::debug::warn!(target: $target, $($arg)+);
-        );
-        ($($arg:tt)+) => (
-            $crate::warn!(target: "runtime", $($arg)+);
-        )
-    }
-
-    #[macro_export]
-    macro_rules! info {
-        (target: $target:expr, $($arg:tt)+) => (
-            frame_support::debug::info!(target: $target, $($arg)+);
-        );
-        ($($arg:tt)+) => (
-            $crate::info!(target: "runtime", $($arg)+);
-        )
-    }
-
-    #[macro_export]
-    macro_rules! debug {
-        (target: $target:expr, $($arg:tt)+) => (
-            frame_support::debug::debug!(target: $target, $($arg)+);
-        );
-        ($($arg:tt)+) => (
-            $crate::debug!(target: "runtime", $($arg)+);
-        )
-    }
-
-    #[macro_export]
-    macro_rules! trace {
-        (target: $target:expr, $($arg:tt)+) => (
-            frame_support::debug::trace!(target: $target, $($arg)+);
-        );
-        ($($arg:tt)+) => (
-            $crate::trace!(target: "runtime", $($arg)+);
-        )
-    }
+macro_rules! trace {
+    (target: $target:expr, $($arg:tt)+) => (
+        frame_support::debug::trace!(target: $target, $($arg)+);
+    );
+    ($($arg:tt)+) => (
+        $crate::trace!(target: RUNTIME_TARGET, $($arg)+);
+    )
 }
